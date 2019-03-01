e4944c2d16bd77c981b7c4d35c51435d51161fca
lieby weintraub <liebahinda@gmail.com>
2019-03-01 05:03:04 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,22 +1,23 @@
-class Shoe
-  attr_accessor :color, :size, :material, :condition
-  attr_reader :brand
+attr_accessor :color, :size, :material, :condition
+  attr_reader :brand	  
 
- BRANDS=[]
-  
-  def initialize(brand)
-    @brand = brand
+
+  BRANDS=[]
+
+   def initialize(brand)	  
+     @brand = brand
     if !(BRANDS.include?(brand))
     BRANDS << brand
     end
-  end
+  end	  
+  def cobble	  
+    self.condition = "new"	    
+    puts "Your shoe is as good as new!"	    
+  end	
 
-  def cobble
-    self.condition = "new"
-    puts "Your shoe is as good as new!"
-  end
-def brand=(brand)
+
+ def brand=(brand)
   @brand=brand
- 
-end 
-end
+
+ end 
+ end 
